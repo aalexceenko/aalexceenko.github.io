d429308491bb72f1aed226199cd40717b892e6ba
aalexceenko <xnyxnymrik@bk.ru>
2020-06-10 18:47:21 +0300

    pink

--- js/script.js
+++
@@ -1,7 +0,0 @@
-var button=document.querySelector(".page-header__toggle");
-button.style.visibility="visible";
-var men=document.querySelector(".page-header");
-men.classList.toggle("page-header--menu-closed");
-button.addEventListener("click", function(){
-  men.classList.toggle("page-header--menu-closed");
-});
